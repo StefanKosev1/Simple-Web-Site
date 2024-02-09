3de972597a08066ff4ca5e39f158b65ced083c61
Stefan Kosev <skosev25@gmail.com>
2024-02-09 13:36:53 +0200

    the commands

--- solution.js
+++ solution.js
@@ -1,39 +1,97 @@
+let list = [];
 
-document.getElementById('contact').addEventListener('click', (e) => {
+document.getElementById("submitButton").addEventListener("click", processInput);
+document
+  .getElementById("inputBox")
+  .addEventListener("keypress", function (event) {
+    if (event.key === "Enter") processInput();
+  });
+document.getElementById("inputBox").focus();
 
+function processInput() {
+  console.log("added event listener");
+  let cmd = document.getElementById("inputBox").value;
+  let result = processCommand(cmd);
+  if (result) printToTerminal(result);
+  printToTerminal("List: " + list.join(" "));
+  document.getElementById("inputBox").value = "";
+}
 
-﻿document.getElementById('about').addEventListener('click', (e) => {
+function printToTerminal(text) {
+  document.getElementById("terminal").value += text + "\n";
+}
 
-    e.preventDefault();
-    document.querySelectorAll('.page').forEach(x => x.classList.add('hide'));
-    document.querySelectorAll('.nav ul li a').forEach(x => x.classList.remove('new-color'));
-    e.target.classList.add('new-color');
+function processCommand(cmd) {
+  let cmdArgs = cmd.split(" ");
+  cmd = cmdArgs.shift();
+  switch (cmd) {
+    case "append":
+      return append(cmdArgs);
+      break;
+    case "reverse":
+      return reverse(cmdArgs);
+      break;
+    case "sort":
+      return sort(cmdArgs);
+      break;
 
-    document.querySelector('.contact-page').classList.remove('hide');
-});
+    case "roll":
+      if (cmdArgs[0] == "left") {
+        rollLeft();
+      } else if (cmdArgs[0] == "right") {
+        rollRight();
+      } else {
+        return "Error: invalid command";
+      }
+      break;
 
-    document.querySelector('.about-page').classList.remove('hide');
-});
+    case "delete":
+      return deleteFromList(cmdArgs[0]);
+      break;
 
-document.getElementById("home").addEventListener("click", (e) => {
-  e.preventDefault();
-  document.querySelectorAll(".page").forEach((x) => x.classList.add("hide"));
-  document
-    .querySelectorAll(".nav ul li a")
-    .forEach((x) => x.classList.remove("new-color"));
-  e.target.classList.add("new-color");
-  document.querySelector(".home-page").classList.remove("hide");
-});
+    case "end":
+      const inputBox = document.getElementById("inputBox");
+      document
+        .getElementById("submitButton")
+        .removeEventListener("click", processInput);
+      inputBox.replaceWith(inputBox.cloneNode(true));
+      return "Finished";
 
+    case "prepend":
+      return prepend(cmdArgs);
+      break;
 
-document.getElementById("register").addEventListener("click", (e) => {
-  e.preventDefault();
-  document.querySelectorAll(".page").forEach((x) => x.classList.add("hide"));
-  document
-    .querySelectorAll(".nav ul li a")
-    .forEach((x) => x.classList.remove("new-color"));
-  e.target.classList.add("new-color");
-  document.querySelector(".register-page").classList.remove("hide");
-});
+    case "insert":
+      return insert(cmdArgs);
+      break;
 
+    case "count":
+      return count(cmdArgs);
+      break;
 
+    default:
+      return "Error: invalid command";
+      break;
+  }
+}
+
+function append(args) {
+  list = list.concat(args);
+}
+
+function rollLeft() {
+  list.push(list.shift());
+}
+
+function rollRight() {
+  list.unshift(list.pop());
+}
+
+function deleteFromList(index) {
+  if (list[index]) {
+    list.splice(list.indexOf(index), 1);
+  }
+  return `Error: invalid index ${index}`;
+}
+
+// TODO: implement more commands here ...
